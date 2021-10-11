c75ff3dae8ec573cea2482c8e1dadeefbbf75e02
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-10-11 16:08:58 +0200

    added axios request to get the loggedin user on reload

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,15 +11,20 @@ import store from "./store";
 //Import router
 import router from "./router";
 
-import '../css/app.scss'
+import "../css/app.scss";
 
 new Vue({
     el: "#app",
     store,
     router,
     created() {
-        axios.get('http://localhost:8000/sanctum/csrf-cookie');
+        axios.get("http://localhost:8000/sanctum/csrf-cookie");
+        axios.get("/api/users").then(response => {
+            const loggedIn = response.data;
+            if (loggedIn) {
+                this.$store.dispatch("loggedInUser");
+            }
+        });
     },
-    render: (h) => h(App),
+    render: h => h(App)
 });
-
