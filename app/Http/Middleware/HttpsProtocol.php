1d080831c5db51586288ab8ef1b7ed30fceb3586
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-10-20 12:18:51 +0200

    added https protocol

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+
+// the middleware will redirect every request to https if:
+//
+// * The current request comes with no secure protocol (http)
+// * If your environment is equals to production. So, just adjust the settings according to your preferences.
+//
+// more info at: https://stackoverflow.com/questions/28402726/laravel-5-redirect-to-https
+class HttpsProtocol {
+
+    public function handle($request, Closure $next)
+    {
+        $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL);
+
+        if (!$request->secure() && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
